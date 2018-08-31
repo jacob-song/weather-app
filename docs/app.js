7f0c748c1465617f8ec91668a36ca975e921a50d
jacob-song <inbeomsong42@gmail.com>
2018-08-31 10:39:12 -0400

    some minor fix

--- docs/app.js
+++ docs/app.js
@@ -3,12 +3,16 @@ function get_weather() {
     var apiKey = config.MY_DARK_KEY;
     var url = "https://api.forecast.io/forecast/";
 
+    //HTML5 geolocation
+    //TODO: use google maps api for search bar instead of 
+    //asking for location via geolocation
     navigator.geolocation.getCurrentPosition(success, error);
 
     function success(position) {
         lat = position.coords.latitude;
         long = position.coords.longitude;
 
+        //TODO: use google maps api to display location name instead
         location.innerHTML = 
             "Latitude: " + lat + "°\nLongitude: " + long + "°";
         
