5d400d59dcb17fa4b513dad840510e53228ffbd8
jacob-song <isong@umd.edu>
2018-09-03 15:16:06 -0400

    added additional weather info and modified background gradinet colors

--- docs/app.js
+++ docs/app.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
 
     //Retrieves current weather information using dark sky api
     function getWeatherInfo(lat, long) {
-        var temperature;
+        var temperature, realTemp, rainChance, humidity, windspeed;
         var url = "https://api.forecast.io/forecast/" + apiKey + "/" + lat + "," + long + "?callback=?";
 
         $.getJSON(url, function(data) {
@@ -64,19 +64,55 @@ $(document).ready(function() {
             console.log(icon);
             skycons.set(document.getElementById("skycon"), icon);
             skycons.play();
+
+            //make numbers nice
             temperature = Math.round(data.currently.temperature * 10) / 10;
+            realTemp = Math.round(data.currently.apparentTemperature * 10) /10;
+            rainChance = Math.round(data.currently.precipProbability * 100);
+            humidity = Math.round(data.currently.humidity * 100);
+            windspeed = Math.round(data.currently.windSpeed);
+
             $("#location").text(location);
-            $("#temperature").html(temperature + "° F, " + data.currently.precipProbability + ", " + data.currently.humidity + ", " + data.currently.windSpeed);
             $("#weather-condition").html(data.currently.summary);
-            // $("#image").append("<canvas class=" + data.currently.icon + "width='64' height='64'></canvas>");
+            $("#temperature").html(temperature + "° F | " + realTemp + "° F");
+            $("#precip").html("Precipitation: " + rainChance + "%");
+            $("#humidity").html("Humidity: " + humidity + "%");
+            $("#wind").html("Wind: " + windspeed + " MPH");
             handleBackground();
+            // $("#image").append("<canvas class=" + data.currently.icon + "width='64' height='64'></canvas>");
         });
     }
 
+    //kind of messy?
     function handleBackground() {
-        if (icon === "partly-cloudy-night") {
-            document.documentElement.style.setProperty('--color-one', '#ffffff');
-            document.documentElement.style.setProperty('--color-two', '#000000');
+        if (icon === "clear-day") {
+            document.documentElement.style.setProperty('--color-one', '#87bbff');
+            document.documentElement.style.setProperty('--color-two', '#ffc949');
+        } else if (icon === "clear-night") {
+            document.documentElement.style.setProperty('--color-one', '#3d3f4f');
+            document.documentElement.style.setProperty('--color-two', '#091047');
+        } else if (icon === "partly-cloudy-day") {
+            document.documentElement.style.setProperty('--color-one', '#87bbff');
+            document.documentElement.style.setProperty('--color-two', '#C9CACA');
+        } else if (icon === "partly-cloudy-night") {
+            document.documentElement.style.setProperty('--color-one', '#091047');
+            document.documentElement.style.setProperty('--color-two', '#7F706C');
+        } else if (icon === "cloudy") {
+            document.documentElement.style.setProperty('--color-one', '#C9CACA');
+            document.documentElement.style.setProperty('--color-two', '#b3cdef');
+        } else if (icon === "rain") {
+            document.documentElement.style.setProperty('--color-one', '#3C424C');
+            document.documentElement.style.setProperty('--color-two', '#395877');
+        } else if (icon === "snow" || icon === "sleet") {
+            document.documentElement.style.setProperty('--color-one', '#B8B8B8');
+            document.documentElement.style.setProperty('--color-two', '#EEEEEE');
+        } else if (icon === "wind") {
+            document.documentElement.style.setProperty('--color-one', '#4ed3d3');
+            document.documentElement.style.setProperty('--color-two', '#d8e9ff');
+        } else { //fog
+            document.documentElement.style.setProperty('--color-one', '#D3D2D3');
+            document.documentElement.style.setProperty('--color-two', '#a5a5a5');
         }
+
     }
 });
